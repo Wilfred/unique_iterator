0bb1c91678e0c1f9312ec57e2725997b72e5a02c
Wilfred Hughes <me@wilfred.me.uk>
2015-05-30 11:03:59 +0100

    Initial implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
+struct UniqueState<I>
+    where I: Iterator
+{
+    seen: HashSet<I::Item>,
+    underlying: I,
+}
+
+trait Unique: Iterator {
+    fn unique(self) -> UniqueState<Self>
+        where Self::Item: Hash + Eq + Clone,
+              Self: Sized,
+    {
+        UniqueState { seen: HashSet::new(), underlying: self }
+    }
+}
+
+impl<I> Unique for I where I: Iterator {}
+
+impl<I> Iterator for UniqueState<I>
+    where I: Iterator,
+          I::Item: Hash + Eq + Clone,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(x) = self.underlying.next() {
+            if !self.seen.contains(&x) {
+                self.seen.insert(x.clone());
+                return Some(x)
+            }
+        }
+        None
+    }
+}
 #[test]
 fn it_works() {
 }
